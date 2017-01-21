43e4a48a1b7f34e3cdbc7026f6cc2ddce5f0be3e
Robin Millette <robin@millette.info>
2017-01-21 17:44:01 -0500

    Touchups.

--- lodash-vision.js
+++ lodash-vision.js
@@ -1,15 +1,25 @@
 'use strict'
 
 // npm
-const lodash = require('lodash')
+const _ = require('_')
 
 const partials = { }
-const makeTpl = (t) => lodash.template(t, { imports: { partials: partials }, variable: 'data' })
+const makeTpl = (t) => _.template(
+  t,
+  {
+    imports: {
+      partials: partials
+    },
+    variable: 'data'
+  }
+)
 
 module.exports = {
   compile: (template) => {
     const tpl = makeTpl(template)
     return (context) => tpl(context)
   },
-  registerPartial: (name, template) => { partials[name] = makeTpl(template) }
+  registerPartial: (name, template) => {
+    partials[name] = makeTpl(template)
+  }
 }
